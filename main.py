7a94a3dd4f12ec57e8c5b7a7c18f3aa9d9f4a515
Diego09cr <diego.criscuolo09@hotmail.com>
2025-03-11 09:55:20 -0300

    Atualização

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
-import func
-import func.funcoes
-pes = {}
-while True:
-    try:
-        print('-'*50)
-        print(f'{"MENU PRINCIPAL":^50}')
-        print('-'*50)
-        print('1 - Ver pessoas cadastradas')
-        print('2 - Cadastrar nova pessoa')
-        print('3 - Sair do Sistema')
-        print('-'*50)
-        esc = int(input('Sua opção: '))
-        if esc == 1:
-            func.funcoes.mostra(pes)
-        elif esc == 2:
-            n = func.funcoes.adiciona()
-        elif esc == 3:
-            print('Tchau')
-            break
-        else:
+def mostra(x):
+    print('-'*50)
+    print(f'{"PESSOAS CADASTRADAS":^50}')
+    print('-'*50)
+    for i in x:
+        print(f"{i:^50}")
+    print('-'*50)
+    return
+
+def adiciona():
+    pessoa = dict()
+    print('-'*50)
+    print(f'{"NOVO CADASTRO":^50}')
+    print('-'*50)
+    nome = str(input('Nome: '))
+    pessoa["nome"] = nome
+    idade = int(input('Idade: '))
+    pessoa["idade"] = idade
+    print(f'Novo registro de {nome} foi adicionado')
+    return pessoa
+
+def comeca():
+    pes = {}
+    while True:
+        try:
+            print('-'*50)
+            print(f'{"MENU PRINCIPAL":^50}')
+            print('-'*50)
+            print('1 - Ver pessoas cadastradas')
+            print('2 - Cadastrar nova pessoa')
+            print('3 - Sair do Sistema')
+            print('-'*50)
+            esc = int(input('Sua opção: '))
+            if esc == 1:
+                mostra(pes)
+            elif esc == 2:
+                adiciona()
+            elif esc == 3:
+                print('Tchau')
+                break
+            else:
+                print('TRY AGAIN')
+        except ValueError:
             print('TRY AGAIN')
-    except ValueError:
-        print('TRY AGAIN')
+
+pes = {}
+comeca()
